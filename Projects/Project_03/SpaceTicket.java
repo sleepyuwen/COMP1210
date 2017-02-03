11ff1c45a83c92d4a10e0017dc55954d72be818d
Perry <eagle4ever20@gmail.com>
2017-02-03 16:42:54 -0600

    fuck it

--- Projects/Project_03/SpaceTicket.java
+++ Projects/Project_03/SpaceTicket.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 import java.text.DecimalFormat;
 /**
 * 
@@ -7,6 +8,8 @@ import java.text.DecimalFormat;
 * @version 2/2/17
 */
 public class SpaceTicket {
+   static final double STUDENT_DISCOUNT = .25;
+   static final double CHILD_DISCOUNT = .35;
    /**
    * 
    *
@@ -16,12 +19,12 @@ public class SpaceTicket {
       Scanner userInput = new Scanner(System.in);
       
       System.out.print("Enter ticket code: ");
-      
+
+
+      DecimalFormat fmt1 = new DecimalFormat("$###,###.##");
       String x = userInput.nextLine();
-      String price;
-      double priceCon
+      double price;
       char category;
-      double catCon
       String hour;
       String minutes;
       String month;
@@ -29,35 +32,42 @@ public class SpaceTicket {
       String year;
       String seat;
       String description;
-      String cost;
-      double prize;
+      double cost;
+      int prize;
+
       
       if (x.length() >= 25) {
          x = x.trim();
-         price = x.substring(0, 9);
-         category = x.substring(9, 10);
-         if (category == "c") {
+         price = Double.parseDouble(x.substring(0, 8));
+         category = x.charAt(8);
          
-         } else if (category == "s") {
-            cost = price - ;
+         if (category == 'c') {
+            cost = price - (price * CHILD_DISCOUNT);
+         } else if (category == 's') {
+            cost = price - (price * STUDENT_DISCOUNT);
          } else {
             cost = price;
          }
-         hour = x.substring(10, 12);
-         minutes = x.substring(12, 14);
-         month = x.substring(14, 16);
-         day = x.substring(16, 18);
-         year = x.substring(18, 22);
-         seat = x.substring(22, 25);
-         description = x.substring(25);
-         prize = Math.random();
          
-         DecimalFormat fmtP = new DecimalFormat("$###,###.00");
-         DecimalFormat fmtC = new DecimalFormat("$###,###.00"); 
+         
+         hour = x.substring(9, 11);
+         minutes = x.substring(11, 13);
+         
+         month = x.substring(13, 15);
+         day = x.substring(15, 17);
+         year = x.substring(17, 21);
+         
+         seat = x.substring(21, 24);
+         description = x.substring(24);
+         
+         Random gen = new Random();
+         prize = gen.nextInt(999999) + 1; 
                  
          System.out.println("Space Ticket: " + description);
-         System.out.println("Date: " + month + "/" + day + "/" + year + "   Time: " + hour + ":" + minutes + "   Seat: " + seat);
-         System.out.println("Price: " + fmtP.format(price) + "   Category: " + category + "   Cost: " + fmtC.format(cost));
+         System.out.println("Date: " + month + "/" + day + "/" + year 
+            + "   Time: " + hour + ":" + minutes + "   Seat: " + seat);
+         System.out.println("Price: " + fmt1.format(price) 
+            + "   Category: " + category + "   Cost: " + fmt1.format(cost));
          System.out.println("Prize Number: " + prize);
       } else {
          System.out.println("*** Invalid ticket code ***");
