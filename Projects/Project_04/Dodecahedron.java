2e90890ec04ceaf751e568981d5635f63e2b4386
Perry <eagle4ever20@gmail.com>
2017-02-08 17:15:43 -0600

    project 4

---
+++ Projects/Project_04/Dodecahedron.java
@@ -0,0 +1,18 @@
+/**
+*/
+public class Dodecahedron {
+   public Dodecahedron(String label, String color, double edge) {
+      
+   }
+   public void getLabel() {
+      return label;
+   }
+   public setlabel(String label) {
+      if(label /= null) {
+         label = string.trim(label);
+         return true;
+      } else {
+         return label
+      }
+   }
+}
