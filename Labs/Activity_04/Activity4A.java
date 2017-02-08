e705c2cb6339bf3e505a2ac89d2fae190d514f01
Perry <eagle4ever20@gmail.com>
2017-02-08 17:49:59 -0600

    activity 4

--- Labs/Activity_04/Activity4A.java
+++ Labs/Activity_04/Activity4A.java
@@ -1,5 +1,12 @@
+/**
+* main class for activity 4. runns methods from UserInfo.java.
+* @author Perry Bunn
+* @version 2/6/2017
+*/
 public class Activity4A {
-
+   /**
+   * @param args not used
+   */
    public static void main(String[] args) {
       
       UserInfo user1 = new UserInfo("Pat", "Doe");
@@ -11,9 +18,9 @@ public class Activity4A {
       
       UserInfo user2 = new UserInfo("Sam", "Jones");
       System.out.println("\n" + user2);
-      user1.setLocation("Atlanta");
-      user1.setAge(21);
-      user1.logOn();
+      user2.setLocation("Atlanta");
+      user2.setAge(21);
+      user2.logOn();
       System.out.println("\n" + user2);
    }
 }
--- Labs/Activity_04/UserInfo.java
+++ Labs/Activity_04/UserInfo.java
@@ -1,15 +1,21 @@
 /**
+* @author Perry Bunn
+* @version 2/6/2017
 */
 public class UserInfo {
    // instance variables
    private String firstName;
-   String lastName;
-   String location;
-   int age;
-   int status;
+   private String lastName;
+   private String location;
+   private int age;
+   private int status;
    private static final int OFFLINE = 0, ONLINE = 1;
    // constructor
-   public UserInfo(String firstNameIn, String lastNameIn){
+   /**
+   * @param firstNameIn user inputs a first name.
+   * @param lastNameIn user inputs a last name.
+   */
+   public UserInfo(String firstNameIn, String lastNameIn) {
       firstName = firstNameIn;
       lastName = lastNameIn;
       location = "Not Specified";
@@ -17,39 +23,59 @@ public class UserInfo {
       status = OFFLINE;
    }
    // methods
+   /**
+   * @return returns inputs as strings
+   */
    public String toString() {
       String output = "Name: " + firstName + " "
          + lastName + "\n";
       output += "Location: " + location + "\n";
       output += "Age: " + age + "\n";
       output += "Status: ";
-      if(status == OFFLINE) {
+      if (status == OFFLINE) {
          output += "Offline";
       } else {
          output += "online";
       }
       return output;
    }
+   /**
+   * @param locationIn takes input from user for the location
+   */
    public void setLocation(String locationIn) {
       location = locationIn;
    }
+   /**
+   * @param ageIn takes input from the user for the age
+   * @return returns the variable age and isSet
+   */
    public boolean setAge(int ageIn) {
       boolean isSet = false;
-      if(ageIn > 0) {
+      if (ageIn > 0) {
          age = ageIn;
          isSet = true;
       }
       return isSet;
    }
+   /**
+   * @return returns the age
+   */
    public int getAge() {
       return age;
    }
+   /**
+   * @return returns the location
+   */
    public String getLocation() {
       return location;
    }
+   /**
+   */
    public void logOff() {
       status = OFFLINE;
    }
+   /**
+   */
    public void logOn() {
       status = ONLINE;
    }
