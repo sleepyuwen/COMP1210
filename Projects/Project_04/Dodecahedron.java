1c8e28b7cc5d386ac2b38c71178ecf7b51e5e969
Perry <eagle4ever20@gmail.com>
2017-02-09 22:35:51 -0600

    project 4

--- Projects/Project_04/Dodecahedron.java
+++ Projects/Project_04/Dodecahedron.java
@@ -1,4 +1,7 @@
+import java.text.DecimalFormat;
 /**
+* @author Perry Bunn
+* @version 2/9/17
 */
 public class Dodecahedron {
 
@@ -8,6 +11,11 @@ public class Dodecahedron {
    private double edge = 0;
 
    // constuctor   
+   /**
+   * @param labelIn takes input for label in the constructor.
+   * @param colorIn takes input for color in the constructor.
+   * @param edgeIn takes input for the edge in the constructor.
+   */
    public Dodecahedron(String labelIn, String colorIn, double edgeIn) {
       setlabel(labelIn);
       setColor(colorIn);
@@ -15,49 +23,87 @@ public class Dodecahedron {
    }
 
    //methods
+   /**
+   * @return returns the label variable.
+   */
    public String getLabel() {
       return label;
    }
+   /**
+   * @param labelIn takes the labelIn for the method.
+   * @return returns the value of the variable.
+   */
    public String setlabel(String labelIn) {
-      if(label != null) {
+      if (labelIn != null) {
          label = labelIn.trim();
       } else {
          return label;
       }
       return label;
    }
+   /**
+   * @return returns the variable color.
+   */
    public String getColor() {
       return color;
    }
+   /**
+   * @param colorIn takes the colorIn and sets it as the color variable.
+   * @return returns the value for color.
+   */
    public String setColor(String colorIn) {
-      if(color != null) {
+      if (color != null) {
          color = colorIn.trim();
       } else {
          return color;
       }
       return color;
    }
+   /**
+   * @return returns the value for the variable edge.
+   */
    public double getEdge() {
       return edge;
    }
+   /**
+   * @param edgeIn takes the edgein and sets it as the edge variable.
+   * @return returns the value for edge.
+   */
    public boolean setEdge(double edgeIn) {
-      if(edge > 0) {
+      if (edgeIn > 0) {
          edge = edgeIn;
          return true;
       } else {
          return false;
       }
    }
-   public void surfaceArea() {
-      double surfaceArea = 3 * Math.sqrt(25 + 10 * Math.sqrt(5)) * Math.pow(edge, 2);
+   /**
+   * @return returns the value of the surface area for the dodecahdron.
+   */
+   public double surfaceArea() {
+      return 3 * Math.sqrt(25 + 10 * Math.sqrt(5)) * Math.pow(edge, 2);
    }
-   public void volume() {
-      double volume = ((15 + 7 * Math.sqrt(5)) / (4)) * Math.pow(edge, 3);
+   /**
+   * @return returns the value of the volume for the dodecahdron.
+   */
+   public double volume() {
+      return ((15 + 7 * Math.sqrt(5)) / (4)) * Math.pow(edge, 3);
    }
-   public void surfaceAreaToVolumeRatio() {
-      double ratio = surfaceArea / volume;
+   /**
+   * @return returns the value of the ratio between the surface ares and volume.
+   */
+   public double surfaceToVolumeRatio() {
+      return surfaceArea() / volume();
    }
+   /**
+   * @return returns the string to be printed for the main method.
+   */
    public String toString() {
-      
+      DecimalFormat fmt = new DecimalFormat("#,##0.0##");
+      return "Dodecahedron \"" + label + "\" is \"" + color
+         + "\" with 30 edges of length " + edge + " units.\n\t"
+         + "surface area = " + fmt.format(surfaceArea()) + " square units\n\t"
+         + "volume = " + fmt.format(volume()) + " cubic units\n\t"
+         + "surface/volume ratio = " + fmt.format(surfaceToVolumeRatio());
    }
 }
