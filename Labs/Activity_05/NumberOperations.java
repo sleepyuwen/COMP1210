08f87724d890c57154a611b01b81ba197dee5fb2
Perry <eagle4ever20@gmail.com>
2017-02-20 11:20:23 -0600

    adding project 5 material

---
+++ Labs/Activity_05/NumberOperations.java
@@ -0,0 +1,42 @@
+public class NumberOperations {
+   private int number;
+   
+   public NumberOperations(int numberIn) {
+      number = numberIn;   
+   }
+   public int getValue() {
+      return number;
+   }
+   public String oddsUnder() {
+      String output = "";
+      int i = 0;
+      while (i < number) {
+         if(i % 2 != 0) {
+            output += i + "\t";
+         }
+      i++;
+      }
+      return output;
+   }
+   public String powersTwoUnder() {
+      String output = "";
+      int powers = 1;
+      while (powers < number) {
+         output += powers + "\t";
+         powers = powers * 2;
+      }
+      return output;
+   }
+   public int isGreater(int compareNumber) {
+      if (number > compareNumber) {
+         return 1;
+      } else if (number < compareNumber) {
+         return -1;
+      } else {
+         return 0;
+      }
+   }
+   public String toString() {
+      return number + "";
+   }
+}
