d41492bdda2e0727f6af0e0571d934c5928f0be2
Perry <eagle4ever20@gmail.com>
2017-02-20 16:51:46 -0600

    tweaks

--- Labs/Activity_05/NumberOpsDriver.java
+++ Labs/Activity_05/NumberOpsDriver.java
@@ -24,11 +24,16 @@ public class NumberOpsDriver {
                         + "with a space followed by 0:");
    
       // get first user input using in.nextInt()
-      userInput = ;
+      System.out.println("");
+      int userInput = in.nextInt();
       // add a while loop as described below: 
    	// while the input is not equal to 0 
          // add a new NumberOperations object to numOpsList based on user input
          // get the next user input using in.nextInt()
+      while (userInput != 0) {
+         NumberOperations enter = new NumberOperations(userInput);
+         System.out.println("For: " + numOpsList.add(userInput));
+      }
       
       int index = 0;
       while (index < numOpsList.size()) {
@@ -36,7 +41,8 @@ public class NumberOpsDriver {
          System.out.println("For: " + num);
       	// add print statement for odds under num
       	// add print statement for powers of 2 under num
-            
+            System.out.println("Odds under: " + num.oddsUnder());
+            System.out.println("Powers of 2 under: " + num.powersTwoUnder());
          index++;
       }
    }
