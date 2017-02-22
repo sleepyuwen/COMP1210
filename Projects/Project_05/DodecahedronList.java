59a8d90ed00790737e22c8a1c3dea4cc41a04f96
Perry <eagle4ever20@gmail.com>
2017-02-22 11:20:09 -0600

    i dont know what im doing

--- Projects/Project_05/DodecahedronList.java
+++ Projects/Project_05/DodecahedronList.java
@@ -5,9 +5,31 @@ public class DodecahedronList {
    private String label = "";
    ArrayList<DodecahedronObjects> dodObjList = new ArrayList<DodecahedronObjects>();
    
-   public DodecahedronList(ArrayList<Dodecahedron>) {
-      
+   public DodecahedronList(ArrayList<DodecahedronObjects>) {
+            
    }
    
+   public void getName() {
+      return label;
+   }
    
+   public int numberOfDodecahedrons() {
+      if (dodobjList.size() != 0) {
+         return dodobjList.size();
+      } else {
+         return 0;
+      }
+   }
+   
+   public double totalSurfaceArea() {
+      int index = 0;
+      if (dodobjList.size() != 0) {
+         while (index < dodobjList.size()) {
+            Dodecahedron.surfaceArea(dodobjList.get(index));
+            index++;
+         } 
+      } else {
+         return 0;
+      } 
+   }
 }
