157e328bdb569efe351f9ab57ed6f7dcd8cff0d3
Perry <eagle4ever20@gmail.com>
2017-02-22 10:20:51 -0600

    project 5

---
+++ Projects/Project_05/DodecahedronList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+/**
+*/
+public class DodecahedronList {
+   private String label = "";
+   ArrayList<DodecahedronObjects> dodObjList = new ArrayList<DodecahedronObjects>();
+   
+   public DodecahedronList(ArrayList<Dodecahedron>) {
+      
+   }
+   
+   
+}
