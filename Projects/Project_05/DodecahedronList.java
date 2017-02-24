fe94e9434b9d130b216e50430130fcf95b18a0f9
Perry <eagle4ever20@gmail.com>
2017-02-23 18:12:10 -0600

    idk

--- Projects/Project_05/DodecahedronList.java
+++ Projects/Project_05/DodecahedronList.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 */
 public class DodecahedronList {
    private String label;
-   ArrayList<DodecahedronList> dodObjList;
+   ArrayList<Dodecahedron> dodObjList;
    
-   public DodecahedronList(String labelIn, ArrayList<DodecahedronList> dodObjIn) {
+   public DodecahedronList(String labelIn, ArrayList<Dodecahedron> dodObjIn) {
       label = labelIn;
       dodObjList = dodObjIn;
    }
@@ -21,12 +21,32 @@ public class DodecahedronList {
    public double totalSurfaceArea() {
       double total = 0;
       int index = 0;
-         while (index < dodObjList.size()) {
+      while (index < dodObjList.size()) {
             // "try and separate the objects and then find the surfaceArea of that new object" - TA
             // wont compile as is
-            total += dodObjList.get(index).surfaceArea();
-            index++;
-         }
-       return total;
+         total += dodObjList.get(index).surfaceArea();
+         index++;
+      }
+      return total;
+   }
+   
+   public double totalVolume() {
+      double total = 0;
+      int index = 0;
+      while (index < dodObjList.size()) {
+         total += dodObjList.get(index).volume();
+         index++;
+      }
+      return total;
+   }
+   
+   public double averageSurfaceArea() {
+      double total = 0;
+      int index = 0;
+      while (index < dodObjList.size()) {
+         total += ;
+         index++;
+      }
+      return total;
    }
 }
